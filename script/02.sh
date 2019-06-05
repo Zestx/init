0a1c5f830665f70bb284024a2648a20333753c97
z3st <pom@localhost.localdomain>
2019-06-06 00:19:31 +0200

    added script/02 - non tested on a logged in user yet but should work.

---
+++ script/02.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "user to delete: " USER
+read -p "also delete $USER homefolder? (y/n): " DELHOME
+echo "unloging and deleting $USER..."
+
+if [ -z "$(pgrep -u $USER)" ]; then
+	echo "..."
+else
+	while read PROC; do
+		sudo kill -9 $PROC
+	done < $(pgrep -u $USER)
+fi
+sudo userdel $USER
+if [ $DELHOME = "y" ]; then
+sudo rm -rf /home/$USER
+fi
+echo "...user deleted."
