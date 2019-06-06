da902fb8985a75db106909d8ebc12e694add9399
root <root@debian.deb.com>
2019-06-06 21:10:27 +0200

    started 03.sh

---
+++ script/03.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "hello world"
+
+if [conditon]; then
+	cmd -xVf this --iotion
+	rere -rerere
+fi
+
+curl -n --ssl-reqd --mail-from "q.bckrt@gmail.com" --mail-rcpt "q.bckrt@gmail.com" --upload-file backup.tar --user "q.bckrt@gmail.com:password"
