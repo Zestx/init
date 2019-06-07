0e85e3fc63886b907b26dda901b6d817ee9864e9
root <root@debian.deb.com>
2019-06-07 19:55:20 +0200

    added script/03.sh

--- script/03.sh
+++ script/03.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+#a simple backup script
 
-echo "hello world"
+#default backup source and destination
+SRC_BACKUP_DEF="/home/quentin/Documents"
+DST_BACKUP_DEF="/home/quentin/backup"
 
-if [conditon]; then
-	cmd -xVf this --iotion
-	rere -rerere
+#prompting user for custom backup source and destination
+echo "BACKUP TOOL"
+read -p "full path of the directory to backup? (default is /home/quentin/Documents): " SRC_BACKUP_SET
+read -p "full path of the backup destination? (default is /home/quentin/backup): " DST_BACKUP_SET
+if [ -z "$SRC_BACKUP_SET" ]; then
+	SRC_BACKUP=$SRC_BACKUP_DEF
+else
+	SRC_BACKUP=$SRC_BACKUP_SET
 fi
 
-curl -n --ssl-reqd --mail-from "q.bckrt@gmail.com" --mail-rcpt "q.bckrt@gmail.com" --upload-file backup.tar --user "q.bckrt@gmail.com:password"
+if [ -z "$DST_BACKUP_SET" ]; then
+	DST_BACKUP=$DST_BACKUP_DEF
+else
+	DST_BACKUP=$DST_BACKUP_SET
+fi
+
+#create filename and make the backup
+echo "creating the archive.."
+NAME="$(date +%Y-%m-%d)_$(hostname)_backup.tar.bz2"
+sudo tar -cjvf - ${SRC_BACKUP}/* > ${DST_BACKUP}/${NAME}
+if [ -f "${DST_BACKUP}/${NAME}" ]; then
+	echo "..done! :)"
+else
+	echo "something went wrong! :("
+fi
