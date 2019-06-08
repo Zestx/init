528df5f4ea41121833e24060bcbdb655e4d88724
z3st <pom@localhost.localdomain>
2019-06-08 03:36:47 +0200

    minor structure/comments changes on script/03

--- script/03.sh
+++ script/03.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#a simple backup script
+##########################
+# A Simple Backup Script #
+##########################
 
 #default backup source and destination
 SRC_BACKUP_DEF="/home/quentin/Documents"
@@ -9,6 +11,8 @@ DST_BACKUP_DEF="/home/quentin/backup"
 echo "BACKUP TOOL"
 read -p "full path of the directory to backup? (default is /home/quentin/Documents): " SRC_BACKUP_SET
 read -p "full path of the backup destination? (default is /home/quentin/backup): " DST_BACKUP_SET
+
+#getting the right source and destination (depend if custom or default)
 if [ -z "$SRC_BACKUP_SET" ]; then
 	SRC_BACKUP=$SRC_BACKUP_DEF
 else
@@ -22,9 +26,11 @@ else
 fi
 
 #create filename and make the backup
-echo "creating the archive.."
 NAME="$(date +%Y-%m-%d)_$(hostname)_backup.tar.bz2"
+echo "creating the archive.."
 sudo tar -cjvf - ${SRC_BACKUP}/* > ${DST_BACKUP}/${NAME}
+
+#inform user about succes or failure
 if [ -f "${DST_BACKUP}/${NAME}" ]; then
 	echo "..done! :)"
 else
